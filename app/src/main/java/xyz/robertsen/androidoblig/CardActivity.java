8fd408bcea90257b3be3f3fcb605c91dc6e7ccb9
Nikolai Fosså <nikolaidime@gmail.com>
2018-02-26 15:52:40 +0100

    Fix layout for CardActivity

--- app/src/main/java/xyz/robertsen/androidoblig/CardActivity.java
+++ app/src/main/java/xyz/robertsen/androidoblig/CardActivity.java
@@ -2,12 +2,18 @@ package xyz.robertsen.androidoblig;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 
 public class CardActivity extends AppCompatActivity {
+
+    RecyclerView recyclerCardHits;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
+
+        recyclerCardHits = findViewById(R.id.card_recycler_cardHits);
+
     }
 }
