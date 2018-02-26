7be8fd70aa01dc8e00fc063e2cc2d6801d01a03b
Nikolai Fosså <nikolaidime@gmail.com>
2018-02-26 18:31:34 +0100

    Add CardHitAdapter

--- app/src/main/java/xyz/robertsen/androidoblig/CardActivity.java
+++ app/src/main/java/xyz/robertsen/androidoblig/CardActivity.java
@@ -2,11 +2,19 @@ package xyz.robertsen.androidoblig;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class CardActivity extends AppCompatActivity {
 
     RecyclerView recyclerCardHits;
+    CardHitAdapter cardHitAdapter;
+    ArrayList<Card> cardArrayList;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,6 +22,11 @@ public class CardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_card);
 
         recyclerCardHits = findViewById(R.id.card_recycler_cardHits);
+        cardArrayList = new ArrayList<>(Arrays.asList(Card.getExampleData(this)));
+        cardHitAdapter = new CardHitAdapter(this, cardArrayList);
+        recyclerCardHits.setAdapter(cardHitAdapter);
+        recyclerCardHits.setLayoutManager(new LinearLayoutManager(this));
+
 
     }
 }
---
+++ app/src/main/java/xyz/robertsen/androidoblig/CardHitAdapter.java
@@ -0,0 +1,64 @@
+package xyz.robertsen.androidoblig;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by gitsieg on 26.02.18.
+ */
+
+public class CardHitAdapter extends RecyclerView.Adapter<CardHitAdapter.CardHitHolder> {
+
+    Context context;
+    ArrayList<Card> cardArrayList;
+    LayoutInflater inflater;
+
+    public CardHitAdapter(Context context, ArrayList<Card> cardArrayList) {
+        this.context = context;
+        this.cardArrayList = cardArrayList;
+        this.inflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public CardHitAdapter.CardHitHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.card_cardhit_item, parent, false);
+        return new CardHitHolder(view, this);
+    }
+
+    @Override
+    public void onBindViewHolder(CardHitAdapter.CardHitHolder holder, int position) {
+        Card card = cardArrayList.get(position);
+        holder.image.setImageDrawable(card.image);
+        holder.title.setText(card.title);
+        holder.text.setText(card.text);
+    }
+
+    @Override
+    public int getItemCount() {
+        return cardArrayList.size();
+    }
+
+    public class CardHitHolder extends RecyclerView.ViewHolder {
+
+        private final ImageView image;
+        private final TextView title, text;
+        private CardHitAdapter adapter;
+
+        public CardHitHolder(View itemView, CardHitAdapter adapter) {
+            super(itemView);
+
+            image = itemView.findViewById(R.id.card_cardhits_image);
+            title = itemView.findViewById(R.id.card_cardhits_title);
+            text = itemView.findViewById(R.id.card_cardhits_title);
+            this.adapter = adapter;
+        }
+    }
+}
