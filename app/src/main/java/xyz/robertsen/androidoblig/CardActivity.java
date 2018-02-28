be5b87667c5e18811b7924b51f43815a416fb613
brukerfeil <brukerfeil72@gmail.com>
2018-02-28 15:04:12 +0100

    Add Landscape views to CardActvity

--- app/src/main/java/xyz/robertsen/androidoblig/CardActivity.java
+++ app/src/main/java/xyz/robertsen/androidoblig/CardActivity.java
@@ -2,11 +2,14 @@ package xyz.robertsen.androidoblig;
 
 import android.app.SearchManager;
 import android.content.Intent;
+import android.content.res.Configuration;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.View;
 import android.widget.SearchView;
 
 
@@ -15,7 +18,9 @@ import java.util.Arrays;
 import java.util.Collections;
 
 public class CardActivity extends AppCompatActivity {
-
+    /**
+     * Class variables
+     */
     SearchView cardHitSearchView;
     RecyclerView recyclerCardHits;
     CardHitAdapter cardHitAdapter;
@@ -27,18 +32,31 @@ public class CardActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_card);
 
-        cardHitSearchView = (SearchView) findViewById(R.id.cardHitSearchView); // inititate a search view
+        /**
+         * Initalizing
+         */
+        cardHitSearchView = (SearchView) findViewById(R.id.cardHitSearchView);
+        /**
+         * Calls the method setCardHitSearchFocus
+         */
         setCardHitSearchFocus();
         cardArrayList = new ArrayList<>(Arrays.asList(Card.getExampleData(this)));
         cardHitAdapter = new CardHitAdapter(this, cardArrayList);
         recyclerCardHits = findViewById(R.id.card_recycler_cardHits);
         recyclerCardHits.setAdapter(cardHitAdapter);
         recyclerCardHits.setLayoutManager(new LinearLayoutManager(this));
-
+        /**
+         * Checks device orentation, if "landscape" -> Runs SetHorizontalOffsets-method
+         */
+            setRecyclerHorizontalOffsets();
 
         handleIntent(getIntent());
 
     }
+
+    /**
+     * Method for setting focus in cardHitSearchView -> Search active
+     */
     private void setCardHitSearchFocus() {
         cardHitSearchView.setIconifiedByDefault(true);
         cardHitSearchView.setFocusable(true);
@@ -52,6 +70,7 @@ public class CardActivity extends AppCompatActivity {
         super.onNewIntent(intent);
         handleIntent(intent);
     }
+
     private void handleIntent(Intent intent) {
 
         if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
@@ -59,5 +78,24 @@ public class CardActivity extends AppCompatActivity {
             Log.i("SearchQuery", query);
         }
     }
+
+    /**
+     * Sets Horizintal Offsets in RecycleView
+     */
+    private void setRecyclerHorizontalOffsets() {
+        recyclerCardHits.addItemDecoration(new RecyclerView.ItemDecoration() {
+            @Override
+            public void getItemOffsets(Rect outRect, View view, RecyclerView parent, RecyclerView.State state) {
+                super.getItemOffsets(outRect, view, parent, state);
+                int totalWidth = parent.getWidth();
+                int cardWidth = getResources().getDimensionPixelOffset(R.dimen.land_card_width);
+                int sidePad = (totalWidth - cardWidth) / 2;
+                sidePad = Math.max(0, sidePad);
+                outRect.set(sidePad, 0, sidePad, 0);
+            }
+        });
+
+    }
+
 
 }
